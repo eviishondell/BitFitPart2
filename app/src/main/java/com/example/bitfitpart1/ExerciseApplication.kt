fe9e383a0f416284f3775b32f045de37d4993243
eviishondell <elayne297@gmail.com>
2023-10-02 01:25:46 -0400

    Initial commit

---
+++ app/src/main/java/com/example/bitfitpart1/ExerciseApplication.kt
@@ -0,0 +1,10 @@
+// File: ExerciseApplication.kt
+
+package com.example.bitfitpart1
+
+import android.app.Application
+//import com.example.bitfit.data.database.AppDatabase
+
+class ExerciseApplication : Application() {
+    val database: AppDatabase by lazy { AppDatabase.getInstance(this) }
+}
---
+++ app/src/main/java/com/example/bitfitpart1/ExerciseRecord.kt
@@ -0,0 +1,11 @@
+// File: ExerciseRecord.kt
+
+package com.example.bitfitpart1
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+data class ExerciseRecord(
+    val exerciseName: String,
+    val exerciseDuration: Int
+)
